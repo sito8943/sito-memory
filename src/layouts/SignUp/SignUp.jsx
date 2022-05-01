b3f9f348e4d468b57855216f0da97f98ec5aea0a
sito8943 <sito8943@gmail.com>
2022-05-01 11:26:29 -0400

    SignUp

--- src/layouts/SignUp/SignUp.jsx
+++ src/layouts/SignUp/SignUp.jsx
@@ -1,7 +1,44 @@
-import { Box } from "@mui/material";
+// @mui components
+import { Box, Button, Paper } from "@mui/material";
+
+// @mui icons
+import VpnKeyIcon from "@mui/icons-material/VpnKey";
+
+// context
+import { useLanguage } from "../../context/Language";
 
 const SignUp = () => {
-  return <Box></Box>;
+  const { languageState } = useLanguage();
+
+  return (
+    <Paper
+      sx={{
+        padding: "20px",
+        width: "320px",
+        height: "400px",
+        background: "#222333",
+        overflowY: "auto",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100%",
+        }}
+      >
+        <VpnKeyIcon
+          color="info"
+          sx={{ marginBottom: "20px", fontSize: "4rem" }}
+        />
+        <Button variant="contained" href="www.facebook.com">
+          {languageState.texts.Buttons.Login}
+        </Button>
+      </Box>
+    </Paper>
+  );
 };
 
 export default SignUp;
