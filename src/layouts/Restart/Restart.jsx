bd70982f5bfeee80bd1102cc593e901239850e00
sito8943 <sito8943@gmail.com>
2022-05-01 08:50:17 -0400

    adding tooltips

--- src/layouts/Restart/Restart.jsx
+++ src/layouts/Restart/Restart.jsx
@@ -1,36 +1,45 @@
+// tippy
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css"; // optional
+
 // prop-types
 import PropTypes from "prop-types";
 
 // @mui components
-import { Button, Typography, useTheme } from "@mui/material";
+import { Button } from "@mui/material";
 
 // @mui icons
 import RotateLeftIcon from "@mui/icons-material/RotateLeft";
 
+//context
+import { useLanguage } from "../../context/Language";
+
 const Restart = (props) => {
-  const { points } = props;
-  const theme = useTheme();
+  const { languageState } = useLanguage();
+
   return (
-    <Button
-      sx={{
-        position: "fixed",
-        zIndex: 99,
-        top: "10px",
-        left: "10px",
-        padding: "10px",
-        transition: "all 400ms ease",
-        opacity: 0.2,
-        minWidth: "30px",
-        "&:hover": {
-          opacity: 1,
-        },
-      }}
-      variant="contained"
-      color="secondary"
-      onClick={() => window.location.reload()}
-    >
-      <RotateLeftIcon />
-    </Button>
+    <Tippy content={languageState.texts.Tooltips.Restart}>
+      <Button
+        sx={{
+          position: "fixed",
+          zIndex: 99,
+          top: "10px",
+          left: "10px",
+          padding: "10px",
+          transition: "all 400ms ease",
+          opacity: 0.2,
+          minWidth: "30px",
+          "&:hover": {
+            opacity: 1,
+          },
+        }}
+        variant="contained"
+        color="secondary"
+        onClick={() => window.location.reload()}
+      >
+        <RotateLeftIcon />
+      </Button>
+    </Tippy>
   );
 };
 
